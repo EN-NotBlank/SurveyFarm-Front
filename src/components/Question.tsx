a4134cf5d8b9a343703f4a298dbfd5f61b3eddbc
mintuchel <max3627@naver.com>
2024-09-21 20:58:03 +0900

    update: dynamic box sizing

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import AddButton from "../buttons/AddButton";
-import FinishButton from "../buttons/FinishButton";
 import QuestionHeader from "./QuestionHeader";
 import Option from "./Option";
+import FinishButton from "../buttons/FinishButton";
 
 const Question = () => {
-
-    // updateOptions가 호출되면 options에 반영하고 화면 렌더링해라
     const [options, updateOptions] = useState<JSX.Element[]>([]);
 
     const addOption = () => {
@@ -15,12 +13,11 @@ const Question = () => {
     };
 
     return (
-        // height가 options 개수에 따라 변하게 하고 안의 component들을 h-full로 하여 변화에 맞게끔 같이 커지게 만들기
-        <div className="flex border-[3px] border-black h-[20vh] my-4" style={{ height: `${4 + options.length * 4}rem` }}>
-            <div className="w-1/6 h-full border-[2px] border-red-500">
+        <div className="flex border-[3px] border-black my-4" style={{ height: `4 + ${options.length + AddButton.length + FinishButton.length}rem` }}> {/* 높이 설정 */}
+            <div className="w-1/6 border-[2px] border-red-500">
                 사진
             </div>
-            <div className="w-5/6 h-full border-[3px] border-green-500">
+            <div className="w-5/6 border-[3px] border-green-500">
                 <QuestionHeader />
                 <div>
                     {options.map((option) => (
