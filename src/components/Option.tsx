cc9837d0c761a8681deff6f089b983fbd71ce9fe
mintuchel <max3627@naver.com>
2024-09-21 22:29:24 +0900

    update: option

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,7 +1,25 @@
-const Option = () => {
+interface OptionProp {
+    index: number;
+    onDelete: () => void;
+}
+
+const Option: React.FC<OptionProp> = ({ index, onDelete }) => {
     return (
-        <div>
-            옵션입니다!
+        <div className="flex px-2 py-2">
+            <h1 className="text-xl font-bold px-2">
+                {index}
+            </h1>
+            <input
+                id="in"
+                type="text"
+                className="w-3/5 mx-2 text-sm leading-none font-medium border border-gray-300 dark:border-gray-600 rounded-md px-2 focus:outline-none focus:border-blue-500 dark:focus:border-white dark:bg-gray-800 dark:text-white"
+                placeholder="선택지를 입력하세요"
+                required
+            ></input>
+            {/* ml-auto로 옆으로 붙이고 마진 줘서 조금 띄워주기*/}
+            <button onClick={onDelete} className="ml-auto mx-5">
+                삭제
+            </button>
         </div>
     );
 };
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddButton from "../buttons/AddButton";
 import QuestionHeader from "./QuestionHeader";
 import Option from "./Option";
@@ -8,17 +8,35 @@ const Question = () => {
     const [options, updateOptions] = useState<JSX.Element[]>([]);
 
     const addOption = () => {
-        const newOption = <Option key={options.length} />; // 새로운 Option 컴포넌트 생성
-        updateOptions((prevOptions) => [...prevOptions, newOption]); // 새로운 옵션 추가
+        if (options.length < 8) {
+            // 새로운 Option 컴포넌트 생성
+            const newOption = (
+                <Option
+                    key={options.length}
+                    index={options.length + 1}
+                    onDelete={() => deleteOption(options.length)}
+                />
+            );
+
+            updateOptions((prevOptions) => [...prevOptions, newOption]); // 새로운 옵션 추가
+        }
     };
 
+    const deleteOption = (index: number) => {
+        // 새로운 배열을 만들어서 특정 인덱스를 제거
+        const newOptions = options.filter((_, i) => i !== index);
+        updateOptions(newOptions);
+        // console.log(options);
+    }
+
     return (
-        <div className="flex border-[3px] border-black my-4" style={{ height: `4 + ${options.length + AddButton.length + FinishButton.length}rem` }}> {/* 높이 설정 */}
-            <div className="w-1/6 border-[2px] border-red-500">
+        <div className="flex border-[3px] border-black my-4 rounded-md" style={{ height: `4 + ${options.length + AddButton.length + FinishButton.length}rem` }}> {/* 높이 설정 */}
+            <div className="w-1/6 border-[4px] border-red-500">
                 사진
             </div>
-            <div className="w-5/6 border-[3px] border-green-500">
+            <div className="w-5/6 border-[4px] border-green-500">
                 <QuestionHeader />
+
                 <div>
                     {options.map((option) => (
                         <div key={option.key} className="border-b border-gray-300 py-2">
@@ -26,6 +44,7 @@ const Question = () => {
                         </div>
                     ))}
                 </div>
+
                 <AddButton onClick={addOption} />
                 {options.length > 0 && <FinishButton />} {/* options.length가 1 이상일 때 FinishButton 렌더링 */}
             </div>
