15dcf3a3b0b328dd816039d4165147c77f6d0acb
huro0906 <huro0906@gmail.com>
2024-09-25 18:04:43 +0900

    init

---
+++ src/layouts/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header_wrap'>
+            <h1>dd</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/__test__/MainTestPage.tsx
@@ -0,0 +1,14 @@
+import Header from "../../layouts/header/header";
+
+const MainTestPage = () =>{
+    return (
+        <div>
+            <Header>
+            </Header>
+            <h1>router Test!</h1>
+        </div>
+        
+    )
+}
+
+export default MainTestPage;
